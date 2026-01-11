f26d915eba10975f9b2b547e45954fae89b65b46
vidhi3000 <vdmaharwade@gmail.com>
2026-01-11 22:49:09 +0530

    fixed Dashboard.jsx

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import Navbar from "../component/Navbar";
 import SkillCard from "../component/SkillCard";
 import StateCard from "../component/StateCard";
-import AddskillDialog from "./Pages/AddskillDialog";
+import AddskillDialog from "./AddskillDialog";
 import { Target, CheckCircle2, Activity, TrendingUp } from "lucide-react";
 export default function Dashboard() {
   const [skills, setSkills] = useState(
